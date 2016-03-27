65a734b3ea30310c0facb99727799e1f557a12a7
Roxanne <roxanne@localhost.localdomain>
2016-03-27 15:27:15 -0500

    Okay that broke, but not images/vid/aud should upload

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,7 +4,7 @@ class Video < ActiveRecord::Base
 	has_attached_file :vid_clip,
     	:path => ":rails_root/public/video/:id/:filename",
    	    :url  => ":s3_domain_url",
-   	    :s3_protocol => 'https',
+   	    :s3_protocol => 'https"
 
 
     validates_attachment_content_type :vid_clip, :content_type => ['video/mp4',]
