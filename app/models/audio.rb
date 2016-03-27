f9c7586e459182965d31ba130d27dd6ad76a1f45
Roxanne <roxanne@localhost.localdomain>
2016-03-27 15:23:45 -0500

    Finally!

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -3,7 +3,8 @@ class Audio < ActiveRecord::Base
 
 	has_attached_file :aud_clip,
     	:path => ":rails_root/public/audio/:id/:filename",
-    	:url  => "/audio/:id/:filename"
+    	:s3_protocol => 'https',
+  	    :url  => ":s3_domain_url"
 
     validates_attachment_content_type :aud_clip, :content_type => ['audio/mp3',]
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -3,7 +3,9 @@ class Video < ActiveRecord::Base
 
 	has_attached_file :vid_clip,
     	:path => ":rails_root/public/video/:id/:filename",
-   	    :url  => "/video/:id/:filename"
+   	    :url  => ":s3_domain_url",
+   	    :s3_protocol => 'https',
+
 
     validates_attachment_content_type :vid_clip, :content_type => ['video/mp4',]
 
