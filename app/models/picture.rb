66a22adab17030773478ab78f8a27bdf2be7ff4d
Roxanne <roxanne@localhost.localdomain>
2016-03-27 15:20:04 -0500

    Grrrrr

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,7 +4,7 @@ class Picture < ActiveRecord::Base
   has_attached_file :image,
 	:s3_protocol => 'https',
     :path => ":rails_root/public/images/:id/:filename",
-    :url  => "/images/:id/:filename"
+    :url  => ":s3_domain_url"
 
     validates_attachment_content_type :image, :content_type => ['image/jpg', 'image/gif', 'image/jpeg', 'image/png']
 end
